b36c908235a119554935b8a8819764e2d0b23497
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-03-25 19:27:15 +0200

    import models, User and PostForm

--- images/views.py
+++ images/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Profile, Image, Comment, Like
+from .forms import PostForm
 
 # Create your views here.
