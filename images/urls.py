4f697d54dae020a4d7079c2e19f5f54aae6da889
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-03-25 19:25:05 +0200

    add urlpatterns

---
+++ images/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.home, name = 'index'),
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^post/', views.post, name='post'),
+    url(r'^search/', views.search_results, name = 'search_results'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
