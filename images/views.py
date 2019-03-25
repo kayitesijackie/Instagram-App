ed70351b5cda613a9177ae69f686bd463c3df5fd
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-03-25 19:29:31 +0200

    add views of all pages

--- images/views.py
+++ images/views.py
@@ -6,3 +6,54 @@ from .models import Profile, Image, Comment, Like
 from .forms import PostForm
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    images = Image.get_images()
+    return render(request, 'index.html', {"images":images})
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    images = Image.objects.filter(user_id = user)
+    profile = Profile.objects.get(user=request.user)
+
+    return render(request, 'profile.html', {"current_user": current_user,"images": images,"profile": profile})
+
+
+@login_required(login_url='/accounts/login/')
+def post(request):
+
+    current_user = request.user
+    if request.method == 'POST':
+
+        form = PostForm(request.POST, request.FILES)
+
+        if form.is_valid:
+            post = form.save(commit=False)
+            post.name = current_user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'post.html', {"post_form": form})
+
+
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+    if 'query' in request.GET and request.GET['query']:
+        search_term = request.GET.get("query")
+        user = Profile.search_profiles(search_term)
+        images = Image.objects.all()
+        message = f"{search_term}"
+
+        content = {
+            "message": message,
+            "found": user,
+            "images": images,
+        }
+
+        return render(request, 'search.html', content)
+    else:
+        message = "You haven't searched for anyone"
+        return render(request, 'search.html', {"message": message})
