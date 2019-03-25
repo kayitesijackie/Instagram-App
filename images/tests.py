770a41f2b2da006265072f5d93a153e6e7265de4
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-03-25 19:24:27 +0200

    add ImageTest class

--- images/tests.py
+++ images/tests.py
@@ -2,3 +2,18 @@ from django.test import TestCase
 from .models import Profile, Image, Comment, Like
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.image = Image(name = 'Singer', caption = 'Gospel singer in Rwanda')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
+    def test_saving_image(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image.delete_image()
+        images = Image.objects.all()
