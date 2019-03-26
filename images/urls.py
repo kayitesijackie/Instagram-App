10b1427da24517dd2124694e49b31ab679525ae0
Kayitesi Jackie <kayitesijacqueline@gmail.com>
2019-03-26 14:49:11 +0200

    change home views to index

--- images/urls.py
+++ images/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.home, name = 'index'),
+    url(r'^$', views.index, name = 'index'),
     url(r'^profile/', views.profile, name='profile'),
     url(r'^post/', views.post, name='post'),
     url(r'^search/', views.search_results, name = 'search_results'),
